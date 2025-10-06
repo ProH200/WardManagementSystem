da2280e80241a2b133a4ddd031ece9186bca4d4f
pkay8 <pkay8@MNTUNGWA>
2025-10-06 10:53:22 +0200

    update connection string

---
+++ Migrations/20251006084926_Server.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wellness_Wardens_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class Server : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
