a4a88885ca1ec4c6497b7e407d146145d47eb794
pkay8 <pkay8@MNTUNGWA>
2025-10-30 11:02:20 +0200

    added sorting patient names and routing in help controller

--- Controllers/HelpController.cs
+++ Controllers/HelpController.cs
@@ -2,18 +2,22 @@
 
 namespace Wellness_Wardens_Project.Controllers
 {
+    [Route("grp-03-20/Help")]
     public class HelpController : Controller
     {
+        [Route("Help")]
         public IActionResult Help()
         {
             return View();
-        }
+        }9
 
+        [Route("FAQ")]
         public IActionResult FAQ()
         {
             return View();
         }
 
+        [Route("Support")]
         public IActionResult Support()
         {
             return View();
