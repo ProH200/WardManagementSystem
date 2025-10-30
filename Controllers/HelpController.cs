8fa3ca09adcc4e9560bd2262dbc6c5264adafc79
noxolo <noxol@NOXOLO>
2025-10-30 11:38:43 +0200

    removed routing

--- Controllers/HelpController.cs
+++ Controllers/HelpController.cs
@@ -2,22 +2,18 @@
 
 namespace Wellness_Wardens_Project.Controllers
 {
-    [Route("grp-03-20/Help")]
     public class HelpController : Controller
     {
-        [Route("Help")]
         public IActionResult Help()
         {
             return View();
         }
 
-        [Route("FAQ")]
         public IActionResult FAQ()
         {
             return View();
         }
 
-        [Route("Support")]
         public IActionResult Support()
         {
             return View();
